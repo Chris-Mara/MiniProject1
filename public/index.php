b52ddbe0879f91ed8575f69a15146c9e866228bc
Chris-Mara <cm395@njit.edu>
2019-06-17 22:56:55 -0400

    Task: Added Test CSV file and index.php boilerplate

--- public/index.php
+++ public/index.php
@@ -1,5 +1,24 @@
 <?php
 
-echo 'test';
+
+//Main Class
+class main {
+
+}
+
+//Class for iterating through the
+class csv {
+
+}
+
+//Class for creating the html of the table
+class html {
+
+}
+
+//Class for printing our the page produced
+class page {
+
+}
 
 ?>
