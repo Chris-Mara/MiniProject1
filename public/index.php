2da8c2f1a6931171d24e9cb1f3a604caa778704a
Chris-Mara <cm395@njit.edu>
2019-06-17 23:13:25 -0400

    Task: Created basic functionality for grabbing data from csv files.

--- public/index.php
+++ public/index.php
@@ -8,7 +8,32 @@ class main {
 
 //Class for iterating through the
 class csv {
+    static public function getRecords($filename) {
+        $file = fopen($filename, "r");
 
+        while(! feof($file)) {
+            $record = fgetcsv($file);
+
+            $records[] = $record;
+        }
+
+        fclose($file);
+        return $records;
+
+    }
+}
+
+//Class for a row or 'record' in the table
+class record {
+
+}
+
+//Class for creating record objects
+class recordFactory {
+    public static function create(Array $array) {
+        $record = new record();
+        return $record;
+    }
 }
 
 //Class for creating the html of the table
